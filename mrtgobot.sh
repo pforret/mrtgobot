28c8279fd0e6065c39f694a263f8f7b0b928b2cd
Peter Forret <peter@forret.com>
2015-03-16 12:46:31 +0100

    wrapper script

--- mrtgobot.sh
+++ mrtgobot.sh
@@ -1,57 +1,11 @@
 #!/bin/sh
-# program requires:
-# * mrtg
-# * awk
-# * can run on sh/bash/ash (e.g. Synology/QNAP)
+# just a wrapper to start the php program
+DIR=$(dirname $0)
+PROG=$(basename $0 .sh)
+REAL=$(cd $DIR; pwd)
 
-#set -ex
-PROGNAME=mrtgobot
-PROGVERSION=1.0
-PROGDATE="2015-01-29"
-
-if [ "$1" == "" ] ; then
-# USAGE
-cat <<END >&2
-=== $PROGNAME $PROGVERSION - $PROGDATE
-=== USAGE
-$PROGNAME index [file.cfg|folder]: create index file for file.cfg or for all .cfg files in a folder
-
-$PROGNAME run   [file.cfg|folder]: run mrtg on file.cfg or for all .cfg files in a folder
-
-$PROGNAME rsync [file.cfg|folder]: rsync generated html/png for file.cfg or for all .cfg files in a folder
-
-$PROGNAME probe [metric]: give back a measurement in mrtg format
-
-END
-exit
+if [ -f "$REAL/$PROG.php" ] ; then
+	php "$REAL/$PROG.php" $*
+else
+	echo "ERROR: cannot find program [$REAL/$PROG.php]" >&2
 fi
-
-BNAME=$(basename $0)
-DNAME=$(dirname  $0)
-if [ "$DNAME" == "" ] ; then
-    DNAME=$(pwd)
-fi
-ACTION=$1
-. $DNAME/lib/tools.inc
-
-case "$ACTION" in
-"run")
-. $DNAME/lib/mrtgobot.run.inc
-;;
-
-"index")
-. $DNAME/lib/mrtgobot.index.inc
-;;
-
-"rsync")
-. $DNAME/lib/mrtgobot.rsync.inc
-;;
-
-"probe")
-. $DNAME/lib/mrtgobot.probe.inc
-;;
-
-*)
-echo "ERROR: cannot interpret action [$ACTION] - use run/index/rsync/probe" >&2
-;;
-esac
